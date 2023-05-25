bdbe41fb19fdfd8550f9622b5cc1b8d908351180
victor guilherme <victorg.queiroz@hotmail.com>
2023-05-24 23:49:39 -0300

    arrumando o banch master

--- db/migrate/20230525023632_add_references_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :users, :host, null: false, foreign_key: true
-    add_reference :users, :visitor, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,51 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_25_023016) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "places", force: :cascade do |t|
-    t.string "city"
-    t.string "country"
-    t.string "neighbourhood"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trips", force: :cascade do |t|
-    t.string "review"
-    t.bigint "place_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["place_id"], name: "index_trips_on_place_id"
-    t.index ["user_id"], name: "index_trips_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.string "address"
-    t.string "language"
-    t.string "city"
-    t.string "country"
-    t.integer "age"
-    t.string "occupation"
-    t.string "interests"
-    t.string "about"
-    t.integer "classification"
-    t.string "visited_places"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "trips", "places"
-  add_foreign_key "trips", "users"
 end
