ad1d7b1aa5f001b85d5f5369d6045f264147c19b
victor guilherme <victorg.queiroz@hotmail.com>
2023-06-01 21:32:32 -0300

    atualizando o front

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # resources :place
-  root to: "places#index"
+  resources :place
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
