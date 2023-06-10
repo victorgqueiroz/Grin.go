dbca9edd1b58c655bdf838843cf4824869535dc4
victor guilherme <victorg.queiroz@hotmail.com>
2023-06-10 14:02:46 -0300

    att master

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,28 @@
 class ProfilesController < ApplicationController
-  before_action :set_place, only: [:show]
+  before_action :set_place, only: [:show, :edit]
 
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @trips = Trip.where(user: @user)
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def my
     @trips = current_user.trips
     @review = Review.new
     @trip = Trip.new
+    @user = current_user
   end
+
+private
+
+def set_place
+  @user = User.find(params[:id])
+end
 
 
 end
