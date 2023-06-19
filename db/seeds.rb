c47642db93779ca9cf14fb46a587e00aa3ae34fa
Felipe Abdo <felipe.abdo@gmail.com>
2023-06-19 06:49:58 -0300

    teste

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,25 @@
-  # require 'pry-byebug'
-  require "city-state"
+# require 'pry-byebug'
+require "city-state"
 
-  # Trip.destroy_all # limpa o db
-  # User.destroy_all # limpa o db
-  # Place.destroy_all # limpa o db
+# Trip.destroy_all # limpa o db
+# User.destroy_all # limpa o db
+# Place.destroy_all # limpa o db
+# Message.destroy_all
 
 6.times do
-  # puts 'creating users'
-  # debugger
-  random_country = CS.countries.to_a.sample
-  states = CS.states(random_country.first.downcase).to_a.sample
-  city = CS.cities(states.first.downcase, random_country.first.downcase).first
+# puts 'creating users'
+# debugger
+random_country = CS.countries.to_a.sample
+states = CS.states(random_country.first.downcase).to_a.sample
+city = CS.cities(states.first.downcase, random_country.first.downcase).first
 
-  user = User.create!(
+place = Place.new(
+  city: city,
+  country: random_country.last,
+  neighborhood: Faker::Address.community
+  )
+  20.times do
+    user = User.create!(
     name: Faker::Name.name,
     email: Faker::Internet.unique.email,
     address: Faker::Address.full_address,
@@ -26,23 +33,20 @@
     nacionality: Faker::Nation.nationality,
     host: Faker::Boolean.boolean,
     visitor: Faker::Boolean.boolean,
-    password: "12345678"
+    password: "12345678",
+    place: place
   )
   # puts 'creating places'
   10.times do
-    place = Place.create!(
-      city: city,
-      country: random_country.last,
-      neighborhood: Faker::Address.community
-      )
-      # puts 'creating trip'
-
-    # Cria a viagem associada ao usuário e ao lugar
     Trip.create!(
       user: user,
       place: place,
       legend: Faker::Lorem.paragraph
       # Cria alguns locais para cada usuário
     )
+    # puts 'creating trip'
+
+    # Cria a viagem associada ao usuário e ao lugar
+    end
   end
 end
