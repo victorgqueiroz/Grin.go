0dede8559ab1d0ca47924de7e31b87dc4db55b31
victor guilherme <victorg.queiroz@hotmail.com>
2023-06-20 00:05:11 -0300

    a

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,6 +5,11 @@ class ChatroomsController < ApplicationController
     @message = Message.new
   end
 
+  def new
+    @chatroom = Chatroom.find(params[:id])
+    @message = Message.new
+  end
+
   def create
     chatroom = (Chatroom.where(first_user: current_user, second_user: User.find([params[:user_id]])).or(Chatroom.where(second_user: current_user, first_user: User.find([params[:user_id]])))).first
     if chatroom.blank?
